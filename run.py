da465ef7558f90cf4aad83259915f16b95f15077
Nirmal Subramani <kumar.n.subramani@gmail.com>
2023-07-20 11:44:49 +0000

    Run the program to test if its iterating as expected

--- run.py
+++ run.py
@@ -2,30 +2,30 @@ questions = {
  "What do elephants use their trunk for?: ": "D",
  "What element does the chemical symbol Au stand for?: ": "D",
  "What is the most visited tourist attraction in the world?: ": "A",
- "What is the only food that cannot go bad?: ": "D"
- "Who invented the gramophone?: ": "B"
- "What is the world’s smallest bird?: ": "D"
- "What is the capital city of India?: ": "B"
- "Who founded Amazon?: ": "C"
- "What animal features in the logo for the World Wildlife Fund?: ": "D"
+ "What is the only food that cannot go bad?: ": "D",
+ "Who invented the gramophone?: ": "B",
+ "What is the world’s smallest bird?: ": "D",
+ "What is the capital city of India?: ": "B",
+ "Who founded Amazon?: ": "C",
+ "What animal features in the logo for the World Wildlife Fund?: ": "D",
  "What is the national bird of India?: ": "C"
 }
 
 options = [["A. Smell", "B. Carry food", "C. Communication", "D. All of the above"],
           ["A. Silver", "B. Magnesium", "C. Salt", "D. Gold"],
           ["A. Eiffel Tower", "B. Statue of Liberty", "C. Great Wall of China", "D. Colosseum"],
-          ["A. Dark chocolate","B. Peanut butter", "C. Canned tuna", "D. Honey"]
-          ["A. Thomas Edison","B. Emile Berliner", "C. Albert Einstein", "D. Isaac Newton"]
-          ["A. Peacock","B. Pigeon", "C. Bee Hummingbird", "D. Eagle"]
-          ["A. Bangalore","B. Delhi", "C. Chennai", "D. Kerala"]
-          ["A. Elon Musk","B. Richard Branson", "C. Jeff Bezos", "D. Narayan Murthy"]
-          ["A. Elephant","B. Tiger", "C. Lion", "D. Panda"]
-          ["A. Elephant","B. Tiger", "C. Lion", "D. Panda"]
+          ["A. Dark chocolate","B. Peanut butter", "C. Canned tuna", "D. Honey"],
+          ["A. Thomas Edison","B. Emile Berliner", "C. Albert Einstein", "D. Isaac Newton"],
+          ["A. Peacock","B. Pigeon", "C. Bee Hummingbird", "D. Eagle"],
+          ["A. Bangalore","B. Delhi", "C. Chennai", "D. Kerala"],
+          ["A. Elon Musk","B. Richard Branson", "C. Jeff Bezos", "D. Narayan Murthy"],
+          ["A. Elephant","B. Tiger", "C. Lion", "D. Panda"],
+          ["A. Elephant","B. Tiger", "C. Lion", "D. Panda"],
           ["A. Eagle","B. Pigeon", "C. Peacock", "D. Rooster"]]
 
 def start_game():
 
-    guesses[]
+    guesses= []
     score = 0
     question_index = 0
 
@@ -38,8 +38,7 @@ def start_game():
         guess = guess.upper()
         guesses.append(guess)
         score += check_answer(questions.get(key), guess)
-        question_index += 1
-    
+        question_index += 1    
     show_score(score, guesses)
 
 
@@ -54,9 +53,13 @@ def check_answer():
 
 
 def show_score():
+    pass
 
 
-def play_again()
+def play_again():
+    pass
+start_game()
+ 
 
 
 
